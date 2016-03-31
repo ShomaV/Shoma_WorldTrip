7be93a8994129617ca4a26cb6fd76fb185182f14
Shoma Gujjar <shoma.gujjar@valtech.co.uk>
2016-03-31 14:41:47 +0100

    gulp adding

--- theWorld/src/theWorld/wwwroot/js/tripEditorController.js
+++ theWorld/src/theWorld/wwwroot/js/tripEditorController.js
@@ -9,8 +9,10 @@
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+        vm.newStop = {};
+        var url = "/api/trips/" + vm.tripName + "/stops";
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        $http.get(url)
             .then(function(response) {
                 angular.copy(response.data, vm.stops);
                     _showMap(vm.stops);
@@ -20,14 +22,37 @@
             })
         .finally(function() {
                 vm.isBusy = false;
-            });
+        });
+
+        vm.addStop = function() {
+            vm.isBusy = true;
+            $http.post(url, vm.newStop)
+                .then(function() {
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                    },
+                    function() {
+                        vm.errorMessage = "Failed to add new stop";
+                    })
+                .finally(function() {
+                    vm.isBusy = false;
+                });
+        };
     }
 
     function _showMap(stops) {//private function starts with _
         if (stops && stops.length > 0) {            
             //show map
+            var mapStops = _.map(stops,function(item) {
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info:item.name
+                }
+            });
             travelMap.createMap({
-                stops: stops,
+                stops: mapStops,
                 selector: "#map",
                 currentStop: 1,
                 initialZoom:3
